a944896f2c6b2c47d0e8198a0b91cf0f92daf126
wbb1977 <wb@illogical.de>
2016-04-28 13:09:54 +0200

    *Updated ASAP to ver 3.2.0

--- modo/src/main/jni/asap/Android.mk
+++ modo/src/main/jni/asap/Android.mk
@@ -5,7 +5,7 @@
    LOCAL_C_INCLUDES += $(NDK_APP_PROJECT_PATH)/jni/unzip   
    LOCAL_SHARED_LIBRARIES := libunzip
    LOCAL_MODULE    := asap
-   LOCAL_SRC_FILES := aatr.c asap.c astil.c asapnative.c
+   LOCAL_SRC_FILES := asap.c asapnative.c
 
    include $(BUILD_SHARED_LIBRARY)
 
--- modo/src/main/jni/asap/asap.h
+++ modo/src/main/jni/asap/asap.h
@@ -146,7 +146,7 @@ void ASAPInfo_Delete(ASAPInfo *self);
 /**
  * Short credits for ASAP.
  */
-#define ASAPInfo_CREDITS  "Another Slight Atari Player (C) 2005-2013 Piotr Fusik\nCMC, MPT, TMC, TM2 players (C) 1994-2005 Marcin Lewandowski\nRMT player (C) 2002-2005 Radek Sterba\nDLT player (C) 2009 Marek Konopka\nCMS player (C) 1999 David Spilka\nFC player (C) 2011 Jerzy Kut\n"
+#define ASAPInfo_CREDITS  "Another Slight Atari Player (C) 2005-2014 Piotr Fusik\nCMC, MPT, TMC, TM2 players (C) 1994-2005 Marcin Lewandowski\nRMT player (C) 2002-2005 Radek Sterba\nDLT player (C) 2009 Marek Konopka\nCMS player (C) 1999 David Spilka\nFC player (C) 2011 Jerzy Kut\n"
 
 /**
  * Returns author's name.
@@ -336,6 +336,11 @@ cibool ASAPInfo_SetAuthor(ASAPInfo *self, const char *value);
 cibool ASAPInfo_SetDate(ASAPInfo *self, const char *value);
 
 /**
+ * Sets the 0-based index of the "main" song.
+ */
+cibool ASAPInfo_SetDefaultSong(ASAPInfo *self, int song);
+
+/**
  * Sets length of the specified song.
  * The length is specified in milliseconds. -1 means the length is indeterminate.
  */
@@ -367,7 +372,7 @@ cibool ASAPInfo_SetTitle(ASAPInfo *self, const char *value);
 /**
  * ASAP version as a string.
  */
-#define ASAPInfo_VERSION  "3.1.5"
+#define ASAPInfo_VERSION  "3.2.0"
 
 /**
  * ASAP version - major part.
@@ -377,17 +382,17 @@ cibool ASAPInfo_SetTitle(ASAPInfo *self, const char *value);
 /**
  * ASAP version - micro part.
  */
-#define ASAPInfo_VERSION_MICRO  5
+#define ASAPInfo_VERSION_MICRO  0
 
 /**
  * ASAP version - minor part.
  */
-#define ASAPInfo_VERSION_MINOR  1
+#define ASAPInfo_VERSION_MINOR  2
 
 /**
  * Years ASAP was created in.
  */
-#define ASAPInfo_YEARS  "2005-2013"
+#define ASAPInfo_YEARS  "2005-2014"
 
 /**
  * Writes text representation of the given duration.
